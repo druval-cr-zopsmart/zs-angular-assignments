4339e27ede5d35529c5f012833fe84479aefe5da
Druval CR <druval.cr@zopsmart.com>
2021-07-19 14:14:23 +0530

    minor change

--- Assignment 4,5/counter-project/src/app/components/header/header.component.ts
+++ Assignment 4,5/counter-project/src/app/components/header/header.component.ts
@@ -8,14 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   payload: number = 0;
   payloadOptions: { label: string, value: number }[] = [
-    { label: 'One', value: 1}, 
-    { label: 'Two', value: 2}, 
-    { label: 'Five', value: 5}
+    { label: 'One', value: 1 }, 
+    { label: 'Two', value: 2 }, 
+    { label: 'Five', value: 5 }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
